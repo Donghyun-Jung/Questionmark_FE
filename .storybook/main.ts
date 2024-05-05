c16bef5d6f1b77cbc93f35cfa7ac59245c767bd8
hjlee2778 <lhj2778@dongguk.edu>
2024-05-05 10:24:31 +0900

    Tap 컴포넌트 및 스토리북

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,3 @@
-import path from 'path';
 import type { StorybookConfig } from '@storybook/nextjs';
 
 const config: StorybookConfig = {
@@ -16,35 +15,5 @@ const config: StorybookConfig = {
   docs: {
     autodocs: 'tag',
   },
-  webpackFinal: async (config) => {
-    if (config.resolve) {
-      config.resolve.alias = {
-        ...config.resolve.alias,
-        '@': path.resolve(__dirname, '../src'),
-      };
-    }
-
-    /// Storybook 에서 SVG 다루기 위한 설정
-    const imageRule = config.module?.rules?.find((rule) => {
-      const test = (rule as { test: RegExp }).test;
-
-      if (!test) {
-        return false;
-      }
-
-      return test.test('.svg');
-    }) as { [key: string]: any };
-
-    imageRule.exclude = /\.svg$/;
-
-    config.module?.rules?.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack'],
-    });
-
-    return config;
-  },
-  staticDirs: ['../public'],
 };
-
 export default config;
