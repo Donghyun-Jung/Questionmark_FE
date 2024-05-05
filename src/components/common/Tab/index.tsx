9147f8d55a0826a7af31f255bc6b5fd8439610da
hjlee2778 <lhj2778@dongguk.edu>
2024-05-05 16:48:40 +0900

    chore: 기본사항 수정

--- src/components/common/Tab/index.tsx
+++ src/components/common/Tab/index.tsx
@@ -1,20 +1,23 @@
 import type { PropsWithChildren } from 'react';
 import * as Styled from './style';
-
 const Tab = (props: PropsWithChildren) => {
   const { children } = props;
-
   return <Styled.Root>{children}</Styled.Root>;
 };
 
 interface MenuProps {
   className?: string;
+  onClick?: () => void;
 }
 
 Tab.Menu = function Menu(props: PropsWithChildren<MenuProps>) {
-  const { className, children } = props;
+  const { className, onClick, children } = props;
 
-  return <Styled.Menu className={className}>{children}</Styled.Menu>;
+  return (
+    <Styled.Menu className={className} onClick={onClick}>
+      {children}
+    </Styled.Menu>
+  );
 };
 
 export default Tab;
